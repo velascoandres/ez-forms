5724f3d8d20e663215eb25a8051ad7643d1e238d
andres velasco <velasco.andrs@gmail.com>
2020-03-23 18:27:57 -0500

    material

--- projects/ez-form/src/lib/ez-form.module.ts
+++ projects/ez-form/src/lib/ez-form.module.ts
@@ -5,16 +5,14 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ToasterModule} from 'angular2-toaster';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatCheckboxModule,
-  MatDatepickerModule,
-  MatIconModule,
-  MatOptionModule,
-  MatRadioModule,
-  MatSelectModule
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import {BsInputComponent} from './componentes-auxiliares/bs-input/bs-input.component';
 import {ImgItemComponent} from './componentes-auxiliares/img-item/img-item.component';
 import {AutoCompleteModule} from 'primeng/autocomplete';
